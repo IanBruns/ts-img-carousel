17600c93713f8ee5d84c16b6a522353f15349a05
Ian Bruns <ian@rbruns.com>
2021-04-16 00:12:52 -0600

    importing photos

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import asyncPhoto from '../../images/asyncAwait.png';
+import belle from '../../images/belle.jpg';
+import ps5 from '../../images/ps5.jpg';
+import taako from '../../images/taako.jpg';
+import thenPhoto from '../../images/thenScreenShot.png';
 
 export default function Carousel(props: any) {
+    const [num, setNum] = useState(0);
+
     return (
         <React.Fragment>
             <h2>Carousel</h2>
