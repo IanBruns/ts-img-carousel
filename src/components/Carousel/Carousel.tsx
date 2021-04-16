62cd4c53c773932452bf2bb4ca527ea97e6ad311
Ian Bruns <ian@rbruns.com>
2021-04-16 00:20:01 -0600

    added images

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import asyncPhoto from '../../images/asyncAwait.png';
 import belle from '../../images/belle.jpg';
 import ps5 from '../../images/ps5.jpg';
-import taako from '../../images/taako.jpg';
+import taako from '../../images/taco.jpg';
 import thenPhoto from '../../images/thenScreenShot.png';
+import Photo from '../Photos/Photo';
 
 export default function Carousel(props: any) {
     const [num, setNum] = useState(0);
 
+    const photoArr = [asyncPhoto, belle, ps5, taako, thenPhoto];
+    const lowerImg = photoArr.map(photo => <Photo source={photo} />)
+
     return (
         <React.Fragment>
             <h2>Carousel</h2>
+            {lowerImg}
         </React.Fragment>
     )
 }
---
+++ src/components/Photos/Photo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Photo(props: any) {
+    return (
+        <img src={props.source} alt='' height='75' width='75' />
+    )
+}
